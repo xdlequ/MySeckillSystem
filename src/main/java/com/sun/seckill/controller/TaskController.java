254cbee6ec2141b120a2d55d292cb73f4b842f20
Warsong <342985461@qq.com>
2019-06-03 02:56:25 +0800

    change

---
+++ src/main/java/com/sun/seckill/controller/TaskController.java
@@ -0,0 +1,40 @@
+package com.sun.seckill.controller;
+
+import com.sun.seckill.model.Goods;
+import com.sun.seckill.model.Task;
+import com.sun.seckill.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+
+@RestController
+public class TaskController {
+    @Autowired
+    private TaskService service;
+
+    @RequestMapping(value = "/task", method = RequestMethod.POST)
+    public String addTask(@RequestBody Task task) {
+        task.setCreateTime(new Timestamp(System.currentTimeMillis()));
+        service.insert(task);
+        return "add task success";
+    }
+
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.GET)
+    public Task selectTask(@PathVariable Integer id) {
+        Task task = service.select(id);
+        return task;
+    }
+
+    @RequestMapping(value = "/task", method = RequestMethod.PUT)
+    public String updateTask(@RequestBody Task task) {
+        service.update(task);
+        return "update task success";
+    }
+
+    @RequestMapping(value = "/task/{id}", method = RequestMethod.DELETE)
+    public String deleteTask(@PathVariable Integer id) {
+        service.delete(id);
+        return "delete task success";
+    }
+}
